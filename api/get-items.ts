3768f9f48b17a6fcf285bb666501e74a8dee4a10
cbasje <45468731+cbasje@users.noreply.github.com>
2021-11-14 00:26:13 +0100

    Update get-items.ts

--- api/get-items.ts
+++ api/get-items.ts
@@ -9,12 +9,6 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 		'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
 	);
 
-	if (!req.body) {
-		res.status(500);
-		res.end();
-		return;
-	}
-
 	var Airtable = require('airtable');
 	Airtable.configure({
 		apiKey: process.env.VITE_AIRTABLE_API_KEY,
@@ -28,7 +22,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 		.firstPage((err: any, records: any) => {
 			if (err) {
 				console.error(err);
-				res.status(501);
+				res.status(500);
 				res.send(err);
 
 				return;
