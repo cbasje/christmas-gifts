16dd3fdf5f762684edea8d9d34e3ed63a2ba251f
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-10-15 21:56:19 +0200

    Fix dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        "@pinia/nuxt",
-        "@nuxtjs/tailwindcss",
-        "@vueuse/nuxt",
-        "@nuxt/content",
-    ],
+    modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
 });
