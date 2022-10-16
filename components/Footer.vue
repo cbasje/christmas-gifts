0056ca72ee9dd0c2dbf16a0284895f35272e35bc
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-10-16 14:02:22 +0200

    Small clean up fixes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-const formatted = useDateFormat(useNow(), "YYYY");
+const year = useDateFormat(useNow(), "YYYY");
 </script>
 
 <template>
     <p class="p-16 text-gray-300 dark:text-gray-700 text-center">
-        &copy; Sebastiaan Benjamins, {{ formatted }}
+        &copy; Sebastiaan Benjamins, {{ year }}
     </p>
 </template>
