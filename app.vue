dea23c6740f9754ded2aca096bd47c073a744138
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-10-16 16:19:37 +0200

    Update manifest for ios

--- app.vue
+++ app.vue
@@ -16,6 +16,8 @@ onMounted(() => {
                 name="description"
                 content="Christmas gifts tracker for Benjamins & Haugens"
             />
+            <Link rel="manifest" href="/app.webmanifest" />
+            <Link rel="apple-touch-icon" href="/images/icon-ios.png" />
         </Head>
 
         <NuxtPage />
