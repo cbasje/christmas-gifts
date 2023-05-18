ff325df9b3f170ec28946922c8a7f928442dae58
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-18 13:27:36 +0200

    Update login.vue

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import "@/styles/main.css";
+</script>
+
 <template>
     <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <main class="max-w-md w-full space-y-8">
