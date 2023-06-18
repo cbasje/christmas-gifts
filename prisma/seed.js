2cce6aeba462013f7126b540a2f8c8c120f8851c
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-06-18 18:19:18 +0200

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,48 +4,68 @@ const prisma = new PrismaClient();
 const users = [
     { name: "Amani", password: "amani12", groups: ["HAUGEN"] },
     { name: "Boudewijn", password: "boudewijn12", groups: ["BENJAMINS"] },
-    { name: "Charis", password: "charis23", groups: ["HAUGEN"] },
+    {
+        name: "Charis",
+        password: "charis23",
+        groups: ["HAUGEN"],
+        partner: "Amani",
+    },
     { name: "George", password: "george34", groups: ["HAUGEN"] },
     { name: "Ingrid", password: "ingrid34", groups: ["BENJAMINS"] },
-    { name: "Jeanne", password: "jeanne45", groups: ["HAUGEN"] },
+    {
+        name: "Jeanne",
+        password: "jeanne45",
+        groups: ["HAUGEN"],
+        partner: "George",
+    },
     { name: "Mika", password: "mika56", groups: ["HAUGEN", "BENJAMINS"] },
-    { name: "Pien", password: "pien34", groups: ["BENJAMINS"] },
+    {
+        name: "Pien",
+        password: "pien34",
+        groups: ["BENJAMINS"],
+        partner: "Boudewijn",
+    },
     {
         name: "Sebastiaan",
         password: "sebas67",
         groups: ["HAUGEN", "BENJAMINS"],
+        partner: "Mika",
     },
-    { name: "Simon", password: "simon23", groups: ["BENJAMINS"] },
+    {
+        name: "Simon",
+        password: "simon23",
+        groups: ["BENJAMINS"],
+        partner: "Ingrid",
+    },
     { name: "Tori", password: "tori78", groups: ["HAUGEN"] },
     { name: "Will", password: "will89", groups: ["HAUGEN"] },
 ];
 
 async function main() {
-    const createdUsers = await Promise.all(
-        users.map(
-            async (u) =>
-                await prisma.user.upsert({
-                    where: { name: u.name },
-                    create: {
-                        name: u.name,
-                        password: u.password,
-                        groups: u.groups,
-                        partner: u.partner
-                            ? {
-                                  connect: {
-                                      where: {
-                                          name: u.partner,
-                                      },
-                                  },
-                              }
-                            : undefined,
-                    },
-                    update: { password: u.password, groups: u.groups },
-                })
-        )
-    );
-
-    console.log({ createdUsers });
+    for await (const u of users) {
+        await prisma.user.upsert({
+            where: { name: u.name },
+            create: {
+                name: u.name,
+                password: u.password,
+                groups: u.groups,
+                partner: u.partner
+                    ? {
+                          connectOrCreate: {
+                              where: {
+                                  name: u.partner,
+                              },
+                              create: {
+                                  name: u.partner,
+                                  password: "",
+                              },
+                          },
+                      }
+                    : undefined,
+            },
+            update: { password: u.password, groups: u.groups },
+        });
+    }
 }
 
 main()
