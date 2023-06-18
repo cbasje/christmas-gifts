8064703deb9cddeed238fba187ca667a767bf040
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-06-18 15:37:10 +0200

    Update prisma schema for auth

---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import { User } from "~/lib/types";
+const prisma = new PrismaClient();
+
+const users: Partial<User>[] = [
+    { name: "Amani", password: "amani12", groups: ["HAUGEN"] },
+    { name: "Boudewijn", password: "boudewijn12", groups: ["BENJAMINS"] },
+    { name: "Charis", password: "charis23", groups: ["HAUGEN"] },
+    { name: "George", password: "george34", groups: ["HAUGEN"] },
+    { name: "Ingrid", password: "ingrid34", groups: ["BENJAMINS"] },
+    { name: "Jeanne", password: "jeanne45", groups: ["HAUGEN"] },
+    { name: "Mika", password: "mika56", groups: ["HAUGEN", "BENJAMINS"] },
+    { name: "Pien", password: "pien34", groups: ["BENJAMINS"] },
+    {
+        name: "Sebastiaan",
+        password: "sebas67",
+        groups: ["HAUGEN", "BENJAMINS"],
+    },
+    { name: "Simon", password: "simon23", groups: ["BENJAMINS"] },
+    { name: "Tori", password: "tori78", groups: ["HAUGEN"] },
+    { name: "Will", password: "will89", groups: ["HAUGEN"] },
+];
+
+async function main() {
+    const createdUsers = await Promise.all(
+        users.map(
+            async (u) =>
+                await prisma.user.upsert({
+                    where: { name: u.name },
+                    create: {
+                        name: u.name!,
+                        password: u.password!,
+                        groups: u.groups,
+                    },
+                    update: { password: u.password, groups: u.groups },
+                })
+        )
+    );
+
+    console.log({ createdUsers });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
