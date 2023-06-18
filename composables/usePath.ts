71b746370b70d791fd91faf20fbeb27ca58f4588
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-06-18 16:44:37 +0200

    Update dependencies

--- composables/usePath.ts
+++ composables/usePath.ts
@@ -1,9 +1,7 @@
-import { joinURL } from "ufo";
-
 export const usePath = () => {
     const fromLocale = (locale: string, path: string) => {
         const newPath = path.replaceAll(/\/(nl|en)/g, "");
-        return joinURL(locale === "nl" ? "nl" : "", newPath);
+        return new URL(newPath, locale === "nl" ? "nl" : "");
     };
 
     return {
