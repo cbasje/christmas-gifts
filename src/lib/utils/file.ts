787a40904a8ec94a7b58cb1db269f15e9f6626fc
Sebastiaan Benjamins <sebastiaan@benjami.in>
2023-11-15 09:55:18 +0100

    Update file.ts

--- src/lib/utils/file.ts
+++ src/lib/utils/file.ts
@@ -8,9 +8,16 @@ export const uploadFile = async (id: string, file: File) => {
 	// Write the file to the static folder
 	// writeFileSync(`static/${file.name}`, Buffer.from(await file.arrayBuffer()));
 
-	// const filename = `${id}.${file.name.split('.').at(-1)}`;
+	const extension = file.name.split('.').at(-1);
+	const contentType = 'image/' + extension?.toLowerCase();
+	console.log('🎄 --------------------------------------🎄');
+	console.log('🎄 ~ uploading:', file.name, contentType);
+	console.log('🎄 --------------------------------------🎄');
+
+	// const filename = `${id}.${}`;
 	const { data, error } = await supabase.storage.from('files').upload(id, file, {
-		upsert: true
+		upsert: true,
+		contentType
 	});
 	if (error) {
 		throw new Error(error.message, { cause: error });
