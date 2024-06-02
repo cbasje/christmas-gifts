860cd75f8341f27a3bca05b9dd386a3005083136
Sebastiaan Benjamins <sebastiaan@benjami.in>
2024-06-02 22:08:26 +0200

    Fix vercel Neon config

--- src/lib/server/drizzle.ts
+++ src/lib/server/drizzle.ts
@@ -1,8 +1,11 @@
 import { dev } from '$app/environment';
 import { neon } from '@neondatabase/serverless';
+import { config } from 'dotenv';
 import { drizzle as drizzleNeon, type NeonHttpDatabase } from 'drizzle-orm/neon-http';
 import { drizzle as drizzlePg, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 import postgres from 'pg';
+
+config({ path: '.env' });
 
 export let db: NodePgDatabase | NeonHttpDatabase;
 if (dev) {
