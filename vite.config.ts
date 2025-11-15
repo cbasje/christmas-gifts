dd7aa19bd4c9fc3837d0e2ac1bb7213f70a515a7
Sebastiaan Benjamins <sebastiaan@benjami.in>
2025-11-15 13:39:10 +0100

    Update styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import icons from 'unplugin-icons/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
 export default defineConfig({
-	plugins: [sveltekit()],
 	define: {
 		SUPERFORMS_LEGACY: true,
 	}
+	plugins: [
+		sveltekit(),
+		icons({
+			compiler: 'svelte',
+			customCollections: {
+				crush: FileSystemIconLoader('./assets/icons', (svg) =>
+					svg.replace(/fill="black"/gi, 'fill="currentColor"')
+				),
+			},
+		}),
+	],
+
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			drafts: {
+				customMedia: true,
+			},
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
+	},
 });
